39ccd38eef314043bff53284dc54aa9f1ba3fc01
Nikita12G <87696400+Nikita12G@users.noreply.github.com>
2021-12-01 21:17:20 +0700

    Added methods for working with the keyboard

--- PersonalInfo/PersonalInfo/TabelCells/PersonTableViewCell.swift
+++ PersonalInfo/PersonalInfo/TabelCells/PersonTableViewCell.swift
@@ -8,26 +8,14 @@
 import UIKit
 
 class PersonTableViewCell: UITableViewCell {
-
-    private var person = Person.getPerson()
+    
+    var personViewController = PersonViewController()
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     @IBAction func deleteButton(_ sender: Any) {
-        
-        removeFromSuperview()
+
     }
     
 }
+
--- PersonalInfo/PersonalInfo/ViewControllers/PersonViewController.swift
+++ PersonalInfo/PersonalInfo/ViewControllers/PersonViewController.swift
@@ -10,29 +10,41 @@ import UIKit
 class PersonViewController: UIViewController {
     
     private var person = Person.getPerson()
-    var personTableViewCell = PersonTableViewCell()
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
     @IBOutlet weak var personInfoTable: UITableView!
-    
     @IBOutlet weak var addChildButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Персональные данные"
-        
+        registerForKeyboardNotifications()
+    }
+    
+    deinit {
+        removeKeyboardNotification()
     }
     
     @IBAction func addChild(_ sender: Any) {
-        AddChilde()
+        addChilde()
     }
     
     @IBAction func clear(_ sender: Any) {
-        person.removeAll()
-        addChildButton.isHidden = false
-        personInfoTable.reloadData()
+        showAlert()
+    }
+    @IBAction func nameAction(_ sender: Any) {
+        nameTextField.resignFirstResponder()
     }
     
-    private func AddChilde() {
-        
+    @IBAction func ageAction(_ sender: Any) {
+        ageTextField.resignFirstResponder()
+    }
+    
+    
+    //    MARK: - Private methods
+    
+    private func addChilde() {
         person.append(.init(name: "", age: ""))
         personInfoTable.reloadData()
         
@@ -41,7 +53,53 @@ class PersonViewController: UIViewController {
             
         }
     }
+    
+    private func clear () {
+        person.removeAll()
+        addChildButton.isHidden = false
+        nameTextField.text = ""
+        ageTextField.text = ""
+        personInfoTable.reloadData()
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(
+            title: "Сбросить данные",
+            message: "Вы уверены что хотите сбросить данные?",
+            preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(
+            title: "Сбросить данные",
+            style: .destructive,
+            handler: { action in self.clear()
+            }))
+        alert.addAction(UIAlertAction(title: "Отмена", style: .default, handler: { action in
+        }))
+        present(alert, animated: true)
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotification () {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func kbWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        let kbFromSize = (userInfo?[PersonViewController.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        personInfoTable.contentOffset = CGPoint(x: 0, y: kbFromSize.height)
+    }
+    
+    @objc func kbWillHide () {
+        personInfoTable.contentOffset = CGPoint.zero
+    }
+    
 }
+//    MARK: - UITableView Data Source
 
 extension PersonViewController: UITableViewDataSource {
     
@@ -55,10 +113,10 @@ extension PersonViewController: UITableViewDataSource {
         
         return cell
     }
-    
-    
-    
 }
+
+//    MARK: - UITableView Delegate
+
 extension PersonViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
@@ -66,11 +124,13 @@ extension PersonViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
             personInfoTable.beginUpdates()
             person.remove(at: indexPath.row)
             personInfoTable.deleteRows(at: [indexPath], with: .fade)
             personInfoTable.endUpdates()
+            
             if person.count < 5 {
                 addChildButton.isHidden = false
             }
